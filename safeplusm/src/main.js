7f6544c5afa3dccae4f4af6f0978f3537a437640
guofeng <17801196234@163.com>
2018-01-11 19:20:14 +0800

    style

--- safeplusm/src/main.js
+++ safeplusm/src/main.js
@@ -13,6 +13,34 @@ Vue.config.productionTip = false
 Vue.use(Vuex);
 Vue.use(axios);
 Vue.use(ElementUI);
+router.beforeEach((to, from, next) => {
+     var flag = IsPC();
+     var href=window.location.href;
+     console.log()
+     if(flag){
+     	
+  		window.location.href=href.replace(/m.secjia.com/,"toutiao.secjia.com")
+     	
+     }else{
+     	next();
+     }
+
+     
+})
+function IsPC() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone",
+                "SymbianOS", "Windows Phone",
+                "iPad", "iPod"];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
